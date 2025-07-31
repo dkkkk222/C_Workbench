a826487a8d022283a3e8381352230170c8ce1d82
wangzhiwei <lucifer890@gmail.com>
2025-07-31 18:20:24 +0800

    1.修改DataGrid样式

---
+++ Workbench/Converters/IsOddAlternationConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Workbench.Converters
+{
+    public class IsOddAlternationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 空值/Unset 防守
+            if (value == null || value == DependencyProperty.UnsetValue)
+                return false;
+
+            int idx;
+            if (value is int) idx = (int)value;
+            else if (!int.TryParse(value.ToString(), out idx)) idx = 0;
+
+            return (idx % 2) == 1; // 奇数行为 true，偶数行为 false
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
