007cc1b5de8b6b93da4a6e1a77c0f3f9b297faeb
wangzhiwei <lucifer890@gmail.com>
2025-08-30 16:50:41 +0800

    1.修改列表布局

--- Workbench/ViewModels/dw/WatchChartListViewModel.cs
+++ Workbench/ViewModels/dw/WatchChartListViewModel.cs
@@ -6,6 +6,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -41,6 +43,82 @@ namespace Workbench.ViewModels.dw
         }
         private static bool IsPlaceholder(WatchChartModel m)
        => m != null && string.Equals(m.Header, "未选中", StringComparison.Ordinal);
+
+        #region 布局控制（与前一页一致）
+
+        private int gridRows = 2;
+        public int GridRows
+        {
+            get => gridRows;
+            set => SetProperty(ref gridRows, value);
+        }
+
+        private int gridColumns = 0; // 0 自动
+        public int GridColumns
+        {
+            get => gridColumns;
+            set => SetProperty(ref gridColumns, value);
+        }
+
+        private bool useWrapLayout = false; // false=UniformGrid, true=WrapPanel
+        public bool UseWrapLayout
+        {
+            get => useWrapLayout;
+            set => SetProperty(ref useWrapLayout, value);
+        }
+
+        private Orientation itemOrientation = Orientation.Horizontal;
+        public Orientation ItemOrientation
+        {
+            get => itemOrientation;
+            set => SetProperty(ref itemOrientation, value);
+        }
+
+        private HorizontalAlignment itemHorizontalAlignment = HorizontalAlignment.Center;
+        public HorizontalAlignment ItemHorizontalAlignment
+        {
+            get => itemHorizontalAlignment;
+            set => SetProperty(ref itemHorizontalAlignment, value);
+        }
+
+        private VerticalAlignment itemVerticalAlignment = VerticalAlignment.Center;
+        public VerticalAlignment ItemVerticalAlignment
+        {
+            get => itemVerticalAlignment;
+            set => SetProperty(ref itemVerticalAlignment, value);
+        }
+
+        private HorizontalAlignment itemContainerHorizontalAlignment = HorizontalAlignment.Stretch;
+        public HorizontalAlignment ItemContainerHorizontalAlignment
+        {
+            get => itemContainerHorizontalAlignment;
+            set => SetProperty(ref itemContainerHorizontalAlignment, value);
+        }
+
+        private VerticalAlignment itemContainerVerticalAlignment = VerticalAlignment.Stretch;
+        public VerticalAlignment ItemContainerVerticalAlignment
+        {
+            get => itemContainerVerticalAlignment;
+            set => SetProperty(ref itemContainerVerticalAlignment, value);
+        }
+
+        // WrapPanel 统一卡片尺寸（可按需用）
+        private double cellWidth = 700;   // 结合 MinWidth=680，略大一些好看
+        public double CellWidth
+        {
+            get => cellWidth;
+            set => SetProperty(ref cellWidth, value);
+        }
+
+        private double cellHeight = 320;  // 结合 MinHeight=300
+        public double CellHeight
+        {
+            get => cellHeight;
+            set => SetProperty(ref cellHeight, value);
+        }
+
+        #endregion
+
         #region Method
         public string Title => string.Empty;
 
--- Workbench/ViewModels/dw/WatchTableListViewModel.cs
+++ Workbench/ViewModels/dw/WatchTableListViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using PPEC.Communication.Model;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -20,9 +22,88 @@ namespace Workbench.ViewModels.dw
             get => _watchGroups;
             set => SetProperty(ref _watchGroups, value);
         }
+        #region ItemsControl 布局控制
+
+        private int gridRows = 2;
+        public int GridRows
+        {
+            get => gridRows;
+            set => SetProperty(ref gridRows, value);
+        }
+
+        private int gridColumns = 0; // 0 表示自动
+        public int GridColumns
+        {
+            get => gridColumns;
+            set => SetProperty(ref gridColumns, value);
+        }
+
+        private bool useWrapLayout = false;
+        /// <summary>
+        /// false = UniformGrid 等分；true = WrapPanel 流式布局
+        /// </summary>
+        public bool UseWrapLayout
+        {
+            get => useWrapLayout;
+            set => SetProperty(ref useWrapLayout, value);
+        }
+
+        private Orientation itemOrientation = Orientation.Horizontal;
+        /// <summary>
+        /// WrapPanel 排布方向（横/竖）
+        /// </summary>
+        public Orientation ItemOrientation
+        {
+            get => itemOrientation;
+            set => SetProperty(ref itemOrientation, value);
+        }
+
+        private HorizontalAlignment itemHorizontalAlignment = HorizontalAlignment.Center;
+        public HorizontalAlignment ItemHorizontalAlignment
+        {
+            get => itemHorizontalAlignment;
+            set => SetProperty(ref itemHorizontalAlignment, value);
+        }
+
+        private VerticalAlignment itemVerticalAlignment = VerticalAlignment.Center;
+        public VerticalAlignment ItemVerticalAlignment
+        {
+            get => itemVerticalAlignment;
+            set => SetProperty(ref itemVerticalAlignment, value);
+        }
+
+        private HorizontalAlignment itemContainerHorizontalAlignment = HorizontalAlignment.Stretch;
+        public HorizontalAlignment ItemContainerHorizontalAlignment
+        {
+            get => itemContainerHorizontalAlignment;
+            set => SetProperty(ref itemContainerHorizontalAlignment, value);
+        }
+
+        private VerticalAlignment itemContainerVerticalAlignment = VerticalAlignment.Stretch;
+        public VerticalAlignment ItemContainerVerticalAlignment
+        {
+            get => itemContainerVerticalAlignment;
+            set => SetProperty(ref itemContainerVerticalAlignment, value);
+        }
+
+        // 如使用 WrapPanel 并希望统一卡片尺寸，可设置这两个值
+        private double cellWidth = 560;
+        public double CellWidth
+        {
+            get => cellWidth;
+            set => SetProperty(ref cellWidth, value);
+        }
+
+        private double cellHeight = 220;
+        public double CellHeight
+        {
+            get => cellHeight;
+            set => SetProperty(ref cellHeight, value);
+        }
+
+        #endregion
 
         private double _tableWidth = 520;   // 初始宽
-        private double _tableHeight = 360;   // 初始高
         public double TableWidth
         { 
             get => _tableWidth; 
@@ -31,6 +112,8 @@ namespace Workbench.ViewModels.dw
                 SetProperty(ref _tableWidth, value);
             }
         }
+
+        private double _tableHeight = 360;   // 初始高
         public double TableHeight
         {
             get => _tableHeight; 
